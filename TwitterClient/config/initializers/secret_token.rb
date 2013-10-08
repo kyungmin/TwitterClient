044ce958e1d1cefbe8e9036dbc23bdb346de4585
App Academy Student <student@appacademy.io>
2013-10-08 15:17:24 -0700

    Started Twitter Client

---
+++ TwitterClient/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterClient::Application.config.secret_token = '8265b15697334d9ff6ac4eddc37ec14aa73b01323d8de2fc31ecccc5a844eb6ed579ba0fc367f3c8cd2e84d5daa0f2b262d4aba6cc37408d2a89299f5be49871'
---
+++ TwitterClient/lib/twitter_session.rb
@@ -0,0 +1,49 @@
+require 'launchy'
+require 'oauth'
+require 'singleton'
+require 'yaml'
+
+class TwitterSession
+  include Singleton
+  attr_reader :access_token
+
+  CONSUMER_KEY = "BAleiIDXVo7zG8uju4g9Lg"
+  CONSUMER_SECRET = "UHu3qCj8Qs1O8eLJPa9eKQGKPfgwEiMCfWElX0q5w"
+  CONSUMER = OAuth::Consumer.new(
+    CONSUMER_KEY, CONSUMER_SECRET, :site => "https://twitter.com")
+
+  def initialize
+    @access_token = read_or_request_access_token
+  end
+
+  def self.get(*args)
+    self.instance.access_token.get(*args)
+  end
+
+  def self.post
+    self.instance.access_token.post(*args)
+  end
+
+  protected
+  def read_or_request_access_token(token_file)
+
+    if File.exist?(token_file)
+      File.open(token_file) { |f| YAML.load(f) }
+
+    else
+      request_token = CONSUMER.get_request_token
+      authorize_url = request_token.authorize_url
+      puts "Go to this URL: #{authorize_url}"
+      Launchy.open(authorize_url)
+
+      puts "Login, and type your verification code in"
+      oauth_verifier = gets.chomp
+
+      access_token = request_token.get_access_token(:oauth_verifier => oauth_verifier)
+      File.open(token_file, "w") { |f| YAML.dump(access_token, f) }
+
+      access_token
+    end
+  end
+
+end
